881cbceafbc8a2d8ad5a4d41e4644d114507b893
Muriel Vieira Teixeira <1305651@ifms.edu.br>
2026-09-25 12:27:32 -0400

    Adiciona COGEN ao portal das coordenacoes

---
+++ supabase/migrations/20260925173000_add_cogen_coordination.sql
@@ -0,0 +1,5 @@
+insert into public.course_coordination_access(email, coordination_name, course_key, is_active, must_change_password)
+values ('cogen.tl@ifms.edu.br', 'Coordenação de Administração', 'tecnico_administracao', true, true)
+on conflict (email, course_key) do update
+set coordination_name = excluded.coordination_name,
+    is_active = true;
